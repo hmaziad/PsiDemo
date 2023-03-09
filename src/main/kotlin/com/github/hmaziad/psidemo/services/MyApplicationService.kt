8326da1e18509d28ad5b4d90495db1d438dcd2c7
GitHub Action <action@github.com>
2023-03-09 08:32:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/hmaziad/psidemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hmaziad/psidemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hmaziad.psidemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hmaziad.psidemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/hmaziad/psidemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hmaziad/psidemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hmaziad.psidemo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hmaziad.psidemo.MyBundle
 
 class MyProjectService(project: Project) {
 
